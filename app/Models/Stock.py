3507c0a2e72272eb9287d32d3ba6c9487fd16207
datablogger-ml <vasubhadra@gmail.com>
2024-06-12 23:11:03 +0530

    Added function for fetching all OHLC data

--- app/Models/Stock.py
+++ app/Models/Stock.py
@@ -6,5 +6,5 @@ class Stock:
         self.ohlc_df = None
         self.stock_name = stock_name
 
-    def fetch_ohlc_data(self, stock_name, start_date=None, end_date=None):
-        self.ohlc_df = yf.download(stock_name, start_date, end_date)
+    def fetch_ohlc_data(self, start_date=None, end_date=None):
+        self.ohlc_df = yf.download(self.stock_name, start_date, end_date)
--- app/api/technical_analysis.py
+++ app/api/technical_analysis.py
@@ -1,3 +1,9 @@
 from app.Models.Stock import Stock
-my_stock = Stock("MSFT")
-print(my_stock.details.income_stmt)
+
+input_name = "MSFT"
+start_date = "2024-01-01"
+end_date = "2024-06-01"
+my_stock = Stock(input_name)
+my_stock.fetch_ohlc_data(start_date, end_date)
+
+print(my_stock.ohlc_df)
