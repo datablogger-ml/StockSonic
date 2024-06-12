9533e774f582e56630142d40beb6b9290704029d
datablogger-ml <vasubhadra@gmail.com>
2024-06-12 23:11:34 +0530

    Adding test code to main module

--- app/api/technical_analysis.py
+++
@@ -1,9 +0,0 @@
-from app.Models.Stock import Stock
-
-input_name = "MSFT"
-start_date = "2024-01-01"
-end_date = "2024-06-01"
-my_stock = Stock(input_name)
-my_stock.fetch_ohlc_data(start_date, end_date)
-
-print(my_stock.ohlc_df)
