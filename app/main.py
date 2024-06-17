f6db935a454724e3e9e878347e21298940561206
datablogger-ml <vasubhadra@gmail.com>
2024-06-17 18:45:43 +0530

    adding variables fo RSI analysis

--- app/main.py
+++ app/main.py
@@ -1,10 +1,24 @@
 from app.Models.Stock import Stock
+from datetime import datetime, timedelta
 
 
 if __name__ == "__main__":
     input_name = "MSFT"
     start_date = "2024-01-01"
     end_date = "2024-06-01"
+
+    end_date = datetime.now().date()
+    start_date = end_date - timedelta(days=365 * 1)
+    incremental_amount = 10_000
+    bollinger_timeframe = 20
+    std_deviation_multiplier = 2
+    raw_data_list = []
+    trade_df_list = []
+
+    rsi_timeframe = 14
+
+    rsi_upper_cap = 80
+    rsi_lower_cap = 30
 
     my_stock = Stock(input_name)
     my_stock.fetch_ohlc_data(start_date, end_date)
