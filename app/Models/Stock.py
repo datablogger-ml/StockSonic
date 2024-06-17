72815798031d2685ba3c729d8b94885b639c7456
datablogger-ml <vasubhadra@gmail.com>
2024-06-17 18:54:38 +0530

    Adding RSI signal dataframe

--- app/Models/Stock.py
+++ app/Models/Stock.py
@@ -3,9 +3,12 @@ import yfinance as yf
 
 class Stock:
     def __init__(self, stock_name):
+        self.rsi_df = None
         self.ohlc_df = None
         self.stock_name = stock_name
 
     def fetch_ohlc_data(self, start_date=None, end_date=None):
         self.ohlc_df = yf.download(self.stock_name, start_date, end_date)
 
+    def create_rsi_signals(self, timeframe=14):
+        self.rsi_df = self.ohlc_df.copy()
